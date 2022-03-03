52a7282104b20589d7045ba1d8c525221c69a04c
cvives-cvent <cvives@cvent.com>
2022-03-03 17:02:04 -0500

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,49 @@
+import re
+from yaml import load
+from yaml import FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter,re.MULTILINE)
+
+    @classmethod
+    def load(self,cls,string):
+        _, fm, content = self.__regex.split(string,2)
+        metadata = load(fm,Loader=FullLoader)
+        return cls(metadata,content)
+
+    def __init__(self,metadata,content):
+        self.data = metadata
+        self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        if "type" in self.data.keys():
+            return self.data["type"]
+        else:
+            return None
+    @type.setter
+    def type(self,type):
+        self.data["type"] = type
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key,value in self.data.items():
+            if key !="content":
+                data[key] = value
+        return str(data)
+
